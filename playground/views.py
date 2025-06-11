b78f2bd2913201ef4196430d6b8e74972b64951e
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-11 12:23:57 +0300

    referencing fields

--- playground/views.py
+++ playground/views.py
@@ -1,18 +1,12 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q,F
 from store.models import Product
 
 
 
 def say_hello(request):
-    #Products:inverntory < 10 and price < 20
-    # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20) 
-    # alternative
-    # queryset = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
-    #Products:inverntory < 10 or price < 20
-    # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20) )
-    queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20) )
+   queryset = Product.objects.filter(inventory=F('collection'))
     
    
-    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
+   return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})

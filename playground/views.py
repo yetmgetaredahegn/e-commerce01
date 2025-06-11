63597db79bb27cbfcea6df6b9d97dd705b816ede
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-11 21:06:47 +0300

    selecting field to query(filter + values)

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q,F
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 
 def say_hello(request):
-   queryset = Product.objects.filter(inventory=F('collection'))
+   queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct().order_by('product__title'))
     
    
    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})

35156225a24fd0d676677ce09684f8f3e02b8c3b
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-12 03:13:41 +0300

    (prefetch_related,select_related)+example

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q,F
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 
 
 def say_hello(request):
    #select_related(1) 1-1 1-n
    # prefetch_related(n) n-n
-   queryset = Product.objects.prefetch_related("promotions").select_related('collection').all()
+   # queryset = Product.objects.prefetch_related("promotions").select_related('collection').all()
+   queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+   
     
    
-   return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
+   return render(request, 'hello.html', {'name': 'Mosh', 'orders': list(queryset)})

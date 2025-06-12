c3798d687393278061853a773d6498984805c34f
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-12 18:59:50 +0300

    Aggregating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q,F
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, OrderItem, Order
 
 
 
 def say_hello(request):
-   #select_related(1) 1-1 1-n
-   # prefetch_related(n) n-n
-   # queryset = Product.objects.prefetch_related("promotions").select_related('collection').all()
-   queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-   
+   result = Product.objects.aggregate(count = Count('id'), min_price=Min('unit_price'))
     
    
-   return render(request, 'hello.html', {'name': 'Mosh', 'orders': list(queryset)})
+   return render(request, 'hello.html', {'name': 'Yetmgeta', 'result': result})

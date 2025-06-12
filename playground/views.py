638054e8919f6967d16954042e14df09aafe4af0
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-13 00:30:40 +0300

    Annotating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.db.models import Value,F,Func
+from django.db.models import Value,F,Func, Count
 from django.db.models.functions import Concat
 from store.models import Product, OrderItem, Order, Customer
 
@@ -10,7 +10,7 @@ def say_hello(request):
       full_name=Func(F('first_name'), Value(' '), F('last_name'),function='CONCAT')
    )
    queryset = Customer.objects.annotate(
-      full_name=Concat('first_name', Value(' '), 'last_name')
+      orders_count=Count('order')
    )
     
    

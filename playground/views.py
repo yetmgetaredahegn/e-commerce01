e9f8f225762a3a53fd30694542f49ac53ac67a64
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-12 20:19:58 +0300

    Annotating Objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.db.models.aggregates import Count, Max, Min, Avg, Sum
-from store.models import Product, OrderItem, Order
+from django.db.models import Value,F
+from store.models import Product, OrderItem, Order, Customer
 
 
 
 def say_hello(request):
-   result = Product.objects.aggregate(count = Count('id'), min_price=Min('unit_price'))
+   queryset = Customer.objects.annotate(new_id=F('id')+1)
     
    
-   return render(request, 'hello.html', {'name': 'Yetmgeta', 'result': result})
+   return render(request, 'hello.html', {'name': 'Yetmgeta', 'result': list(queryset)})

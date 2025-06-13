91d5fa2cc050e84c6bd4ec06147aaf305e8c4ffa
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-13 16:16:04 +0300

    creating objects

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import Product, Collection,OrderItem,Order,Customer
 from tags.models import TaggedItem
 
 def say_hello(request):
-   # custom managers
-   queryset=TaggedItem.objects.get_tags_for(Product,1)
-   
-   return render(request, 'hello.html', {'name': 'Yetmgeta', 'tags': list(queryset)})
+   collection = Collection()
+   collection.title=  "Videp Games"
+   collection.featured_product = Product(pk=1)
+   collection.save()
+
+   Collection.objects.create(title='PC',featured_product_id=2)
+    
+   return render(request, 'hello.html', {'name': 'Yetmgeta'})

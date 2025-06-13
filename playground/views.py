87f89b3e0c518e41e8488aa700201cfc718ea634
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-13 15:45:57 +0300

    custom managers

--- playground/views.py
+++ playground/views.py
@@ -4,12 +4,7 @@ from store.models import Product
 from tags.models import TaggedItem
 
 def say_hello(request):
-   content_type=ContentType.objects.get_for_model(Product)
-   queryset = TaggedItem.objects \
-      .select_related('tag') \
-      .filter( 
-         content_type=content_type,
-         object_id=1
-      )
+   # custom managers
+   queryset=TaggedItem.objects.get_tags_for(Product,1)
    
    return render(request, 'hello.html', {'name': 'Yetmgeta', 'tags': list(queryset)})

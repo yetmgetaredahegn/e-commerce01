f012ad986ea5ed2ca71de0ceb779a27590f47d01
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-15 17:57:03 +0300

    selecting related obj admin site

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,19 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','unit_price']
+    list_display = ['title','unit_price', 'inventory_status','collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self,product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'LOW'
+        return "OK"
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -13,5 +23,14 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name','last_name']
     list_per_page = 10
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer_first_name','payment_status']
+    list_select_related = ['customer']
+
+    @admin.display(ordering='customer')
+    def customer_first_name(self,order):
+        return order.customer.first_name
 
 admin.site.register(models.Collection)

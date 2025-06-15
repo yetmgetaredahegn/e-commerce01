b743627756d6024244afc810f2fbc8a31becff1d
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-16 00:18:49 +0300

    overiding base querset + create links

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.utils.html import format_html
 from . import models
 
 @admin.register(models.Product)
@@ -26,11 +28,25 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['customer_first_name','payment_status']
+    list_display = ['id','customer','payment_status']
     list_select_related = ['customer']
 
     @admin.display(ordering='customer')
-    def customer_first_name(self,order):
+    def customer_em(self,order):
         return order.customer.first_name
+    
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
 
-admin.site.register(models.Collection)
+    @admin.display(ordering='products_count')
+    def products_count(self,collection):
+        return format_html('<a href="http://google.com"> {} </a>', collection.products_count)
+        
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
+
+

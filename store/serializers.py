a3086320e0748aebbea1641266688e678c831a50
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-19 00:18:43 +0300

    Model Serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,24 +1,21 @@
+from dataclasses import field, fields
 from decimal import Decimal
 from rest_framework import serializers
 from store.models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id','title']
+  
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2,source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','unit_price','paying_tax','collection']
+
     paying_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # collection = serializers.PrimaryKeyRelatedField(
-    #     queryset = Collection.objects.all()
-    # )
-    # collection = CollectionSerializer()
-    # collection = serializers.StringRelatedField()
-    collection = serializers.HyperlinkedRelatedField(
-        queryset = Collection.objects.all(),
-        view_name = 'collection-detail'
-    )
+  
+    
     def calculate_tax(self,product: Product):
         return product.unit_price * Decimal(1.1)

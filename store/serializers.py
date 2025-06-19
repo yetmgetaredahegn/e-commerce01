f10a953c5eb16991aa1883732ebd34cb2fa13e62
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-20 00:57:10 +0300

    restful apis

--- store/serializers.py
+++ store/serializers.py
@@ -1,21 +1,25 @@
-from dataclasses import field, fields
 from decimal import Decimal
-from rest_framework import serializers
 from store.models import Product, Collection
+from rest_framework import serializers
+
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id','title']
-  
+        fields = ['id', 'title', 'products_count']
+
+    products_count = serializers.IntegerField()
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id','title','description','slug','inventory','unit_price','paying_tax','collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
 
-    paying_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-  
-    
-    def calculate_tax(self,product: Product):
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
+   

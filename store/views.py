982287a8ffb5342bf66e1be0ba8f10fcd33a2f1a
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-23 02:52:04 +0300

    generic filtering best

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from itertools import product
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
@@ -8,22 +9,23 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
+
+from store.filters import ProductFilter
 from .models import Collection, OrderItem, Product, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['collection_id']
+    # custom filter
+    filterset_class = ProductFilter
     
     def get_serializer_context(self):
         return {'request': self.request}
     
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:

c87a07001d37cb9fce59eaf71bba6f5e2caa1923
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-23 02:17:58 +0300

    filetering behind the hood

--- store/views.py
+++ store/views.py
@@ -17,6 +17,13 @@ class ProductViewSet(ModelViewSet):
     
     def get_serializer_context(self):
         return {'request': self.request}
+    
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params.get('collection_id')
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+        return queryset
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:

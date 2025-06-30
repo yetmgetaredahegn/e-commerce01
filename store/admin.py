4916f8774ce67808d333b9713b693453db5609e0
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-06-30 22:41:55 +0300

    groups and permissions

--- store/admin.py
+++ store/admin.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html, urlencode
 from . import models
-from core.models import User
 
 # custom filters
 class InventoryFilter(admin.SimpleListFilter):
@@ -54,13 +53,11 @@ class ProductAdmin(admin.ModelAdmin):
         #    messages.ERROR
        )
 
-# class UserInline(admin.TabularInline):
-#     model = get_user_model()
+
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     autocomplete_fields = ['user']
-    # inlines = [UserInline]
     list_display = ['first_name','last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     list_per_page = 10
--- store/models.py
+++ store/models.py
@@ -2,7 +2,9 @@ from itertools import product
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.contrib import admin
 from uuid import uuid4
+
 
 
 class Promotion(models.Model):
@@ -61,9 +63,11 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
     
+    @admin.display(ordering='user__first_name')
     def first_name(self):
         return self.user.first_name
     
+    @admin.display(ordering='user__last_name')
     def last_name(self):
         return self.user.last_name
 

b89f609c8776d0ee0a80524dff474b0683fc3880
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-07-21 23:06:44 +0300

    sending emails

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins,BadHeaderError
 
 
 def say_hello(request):
+    try: 
+        mail_admins('subject','message',html_message='message')
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
--- storefront/settings.py
+++ storefront/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from unittest.mock import DEFAULT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -176,3 +177,14 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT =2525
+DEFAULT_FROM_EMAIL = 'from@yetm.com'
+
+ADMINS = [
+    ('Yetu','admin@yetu.com')
+]

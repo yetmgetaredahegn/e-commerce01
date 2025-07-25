bbbe44d863ae09056e439a2dbdfa44bbef742485
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-07-26 01:30:08 +0300

    lowlevel cache api

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,12 @@
+from urllib import response
+from django.core.cache import cache
 from django.shortcuts import render
-from .tasks import notify_customers
-
+import requests
 
 def say_hello(request):
-    notify_customers.delay('Hello')
-    
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    key='httpbin_result'
+    if cache.get(key) is None:
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        cache.set(key,data)
+    return render(request, 'hello.html', {'name': cache.get(key)})

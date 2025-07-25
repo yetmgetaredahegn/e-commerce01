dcd65735c42cf9861a7a52a37d8095e474277e97
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-07-26 01:36:17 +0300

    cache page

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
 from urllib import response
 from django.core.cache import cache
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 import requests
 
+
+@cache_page(5*60)
 def say_hello(request):
-    key='httpbin_result'
-    if cache.get(key) is None:
-        response = requests.get('https://httpbin.org/delay/2')
-        data = response.json()
-        cache.set(key,data)
+    response = requests.get('https://httpbin.org/delay/2')
+    data = response.json()
+        
     return render(request, 'hello.html', {'name': cache.get(key)})

66b02b6b0b1f5f6305c3c4f8a2272643f38dc716
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-07-26 01:51:09 +0300

    cache page  clas based vie

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,21 @@
 from urllib import response
 from django.core.cache import cache
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from rest_framework.views  import APIView 
 import requests
+
+
+class HelloView(APIView):
+    @method_decorator(cache_page(5*60))
+    def get(self,request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        
+        return render(request, 'hello.html', {'name': data})
+
+
 
 
 @cache_page(5*60)
@@ -10,4 +23,4 @@ def say_hello(request):
     response = requests.get('https://httpbin.org/delay/2')
     data = response.json()
         
-    return render(request, 'hello.html', {'name': cache.get(key)})
+    return render(request, 'hello.html', {'name': data})
